f1bb8bba6a41cdc9c2d3ac667b5934eb660fb489
Felipe Almeida <felipeortalmeida@gmail.com>
2020-03-14 19:09:18 -0300

    Tested POST method

--- 14-xhr-custom-lib/app.js
+++ 14-xhr-custom-lib/app.js
@@ -1,11 +1,28 @@
 var http = new XHR();
 
-http.get(
+// http.get(
+//     "https://jsonplaceholder.typicode.com/posts",
+//     function(err) {
+//         console.log(err);
+//     },
+//     function(res) {
+//         console.log(res.data);
+//     }
+// );
+
+const body = {
+    title: "Modern development with Vue and Typescript",
+    body: "Or how a tiny framework has made it into the giants",
+    userId: 1
+}
+
+http.post(
     "https://jsonplaceholder.typicode.com/posts",
+    body,
     function(err) {
         console.log(err);
     },
     function(res) {
-        console.log(res.data);
+        console.log(res);
     }
 );
