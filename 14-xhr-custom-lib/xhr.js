e831c899ad82041009ffec70b85aca9dac3d64c3
Felipe Almeida <felipeortalmeida@gmail.com>
2020-03-14 19:07:36 -0300

    Added POST method

--- 14-xhr-custom-lib/xhr.js
+++ 14-xhr-custom-lib/xhr.js
@@ -17,3 +17,20 @@ XHR.prototype.get = function(url, errorCallback, successCallback) {
     }
     this.http.send();
 };
+
+XHR.prototype.post = function(url, body, errorCallback, successCallback) {
+    this.http.open("POST", url);
+    this.http.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+    this.http.onload = function() {
+        if (this.status >= 200 && this.status < 300) {
+            return successCallback({
+                data: JSON.parse(this.response)
+            });
+        } else {
+            return errorCallback({
+                status: this.status
+            });
+        }
+    }
+    this.http.send(JSON.stringify(body));
+};
