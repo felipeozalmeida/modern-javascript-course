37e1d65468c5281c5fe90d953339cb4d7ca7fb41
Felipe Almeida <felipeortalmeida@gmail.com>
2020-03-18 01:18:33 -0300

    Implemented new POST method

--- 15-http-custom-lib-es6/app.js
+++ 15-http-custom-lib-es6/app.js
@@ -1,33 +1,27 @@
 // var http = new XHR();
 import http from "./lib.js";
 
+// Example object for POST and PUT operations
+const body = {
+    title: "Modern development with Vue and Typescript",
+    body: "Or how a tiny framework has made it into the giants",
+    userId: 55
+}
+
 // GET post
 // http.get("https://jsonplaceholder.typicode.com/posts/1")
 //     .then(res => console.log(res))
 //     .catch(err => console.log(err));
 
 // GET posts
-http.get("https://jsonplaceholder.typicode.com/posts")
+// http.get("https://jsonplaceholder.typicode.com/posts")
+//     .then(res => console.log(res))
+//     .catch(err => console.log(err));
+
+// POST post (:P)
+http.post("https://jsonplaceholder.typicode.com/posts", body)
     .then(res => console.log(res))
     .catch(err => console.log(err));
-
-// var body = {
-//     title: "Modern development with Vue and Typescript",
-//     body: "Or how a tiny framework has made it into the giants",
-//     userId: 1
-// }
-
-// http.post(
-//     "https://jsonplaceholder.typicode.com/posts",
-//     body,
-//     function(err) {
-//         console.log(err);
-//     },
-//     function(res) {
-//         console.log(res.data);
-//         postId = res.data.id;
-//     }
-// );
 
 // http.put(
 //     "https://jsonplaceholder.typicode.com/posts/1",
--- 15-http-custom-lib-es6/lib.js
+++ 15-http-custom-lib-es6/lib.js
@@ -10,22 +10,15 @@ export default class HTTP {
                 .catch(error => reject(error));
         });
     }
-    // post(url, body, errorCallback, successCallback) {
-    //     this.http.open("POST", url);
-    //     this.http.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-    //     this.http.onload = function() {
-    //         if (this.status >= 200 && this.status < 300) {
-    //             return successCallback({
-    //                 data: JSON.parse(this.response)
-    //             });
-    //         } else {
-    //             return errorCallback({
-    //                 status: this.status
-    //             });
-    //         }
-    //     };
-    //     this.http.send(JSON.stringify(body));
-    // }
+    static post(url, body) {
+        return new Promise((resolve, reject) => {
+            fetch(url, { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify(body) })
+                .then(response => Utils.handleErrors(response))
+                .then(validResponse => validResponse.json())
+                .then(json => resolve(json))
+                .catch(error => reject(error));
+        })
+    }
     // put(url, body, errorCallback, successCallback) {
     //     this.http.open("PUT", url);
     //     this.http.setRequestHeader("Content-type", "application/json; charset=UTF-8");
