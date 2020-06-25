7138dd29fb67c3d679e5b7ed646a4f35f5fb4ba6
Felipe Almeida <felipeortalmeida@gmail.com>
2020-06-25 20:49:54 -0300

    Refactored custom HTTP library to use async await

---
+++ 16-http-custom-lib-es7/app.js
@@ -0,0 +1,34 @@
+// var http = new XHR();
+import http from "./lib.js";
+
+// Example object for POST and PUT operations
+const body = {
+    title: "Modern development with Vue and Typescript",
+    body: "Or how a tiny framework has made it into the giants",
+    userId: 55
+}
+
+// GET post
+// http.get("https://jsonplaceholder.typicode.com/posts/1")
+//     .then(res => console.log(res))
+//     .catch(err => console.log(err));
+
+// GET posts
+// http.get("https://jsonplaceholder.typicode.com/posts")
+//     .then(res => console.log(res))
+//     .catch(err => console.log(err));
+
+// POST post (:P)
+// http.post("https://jsonplaceholder.typicode.com/posts", body)
+//     .then(res => console.log(res))
+//     .catch(err => console.log(err));
+
+// PUT post
+// http.put("https://jsonplaceholder.typicode.com/posts/1", body)
+//     .then(res => console.log(res))
+//     .catch(err => console.log(err));
+
+// DELETE post
+http.delete("https://jsonplaceholder.typicode.com/posts/1")
+    .then(() => console.log("Post deleted!"))
+    .catch(err => console.log(err));
